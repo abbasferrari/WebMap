47a201847d547776a1b72f9ec3fcf7e6e56fb8a3
abbasferrari <abbas.ferrari@gmail.com>
2018-09-12 15:54:54 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var router = express.Router();
+
+var fs = require('fs');
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var path = require("path");
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname,"/..")));
+
+app.all('/',function(req,res){
+    res.render(path.join(__dirname ,"/check.html"));
+});
+
+app.post('/updateMap', function(req, res) {
+    var jsonData = req.body.NewJSON;
+    fs.writeFile('neighborhood.geojson', jsonData, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+}
